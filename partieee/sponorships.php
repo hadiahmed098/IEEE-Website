70d4d5bfd8c10657443385baf40966d61e6b4f87
Hadi Ahmed <hadiahmed098@gmail.com>
2020-10-14 15:16:15 -0400

    PART page done, final information needs to be added

--- partieee/sponorships.php
+++ partieee/sponorships.php
@@ -3,85 +3,59 @@
     $site_index = '/partieee';
     include '../header.php';
 ?>
-<style>
-    .center-box{
-        width: 300px;
-        border: 5px solid white;
-        padding: 25px;
-        margin: 25px;
-    }
-</style>
-    <?php
-        include 'navigation.php';
-    ?>
-    <link href="/assets/bootstrap.partieee.css" rel="stylesheet">
-    <!-- Content Row -->
 
-    <div class="fullscreen-bg">
-    <video loop muted autoplay poster="images/Genie.jpg" id="bg-vid" class="fullscreen-bg__video">
-        <source src="videos/backgroundl" type="video/webm">
-    </video>
-    </div>
-    <script type="text/javascript">
-      /* set playback rate for bg video */
-      document.getElementById("bg-vid").playbackRate = .6;
-    </script>
-<div class="well card-1 text-light">
-    <div class="row text-center">
-        <h1 id="events">Sponsors</h1>
-        <p>Without our generous sponsors, it would not be possible for our team to go to competition. We are very thankful for the support provided by our sponsors.
+<!-- Content Row -->
+<div class="well card-1">
+    <div class="row">
+        <!-- Sidebar Column -->
+        <?php include 'navigation.php' ?>
+        <script>
+            let currentLink = "part-sponsership-sidelink";
+            document.getElementsByClassName(currentLink)[0].classList.add("active");
+        </script>
+
+        <!-- Content Column -->
+        <div class="col-md-7 text-dark col-md-offset-1 text-center">
+        <h1>Sponserships</h2>
+        <p>
+        Without our generous sponsers, it would not be possible for our team to go to 
+            competition. We are very thankful to the support provided by our sponsers.
         </p>
-        <br />
-        <img src="images/ECE.png" style="width: 50%">
-        <img src="images/IEEE.png" style="width: 50%">
-        <img src="images/solidworks.png" style="width: 50%">
-        <img src="images/northropgrumman.png" style="width: 50%">
 
-        <h1 id="events">Acknowledgement</h1>
-        <br />
-    </div>
-    <div class="row-text-list" style="padding-left: 30%;">
-        <p>We recognize corporate sponsors in the following ways:</p>
-        <ul>
-            <li>Aerial Robotics Partner Colonel: $3,500</li>
-            <ul>
-            	<li>Recognition in Journal Paper and on Sponsor Board</li>
-            	<li>Logo on Back of Team Shirts</li>
-            	<li>Large Sticker on the Vehicle</li>
-            	<li>Recognition in Official Team Press Release to Media</li>
-            </ul>
-            <li>Aerial Robotics Partner Major: $2,000</li>
-            <ul>
-            	<li>Recognition in Journal Paper and on Sponsor Board</li>
-	            <li>Logo on Back of Team Shirts</li>
-                <li>Small Sticker on the Vehicle</li>
-            </ul>
-            <li>Aerial Robotics Partner Captain: $1,500</li>
-            <ul>
-                <li>Recognition in Journal Paper and on Sponsor Board</li>
-	            <li>Small Sticker on the Vehicle</li>
-            </ul>
-            <li>Aerial Robotics Partner Lieutenant: $500</li>
-            <ul><li>Recognition in Journal Paper and on Sponsor Board</li></ul>
-        </ul>
-    </div>
-    <div class="row text-center">
-        <br /><br />
-        <h1 id="events">Interested In Sponsoring Us?</h1>
-        <br />
-        <p>By supporting Purdue Aerial Robotics Team IEEE, your company will be helping passionate and motivated students gain valuable experience that will enhance their technical and professional skills. The UAS will be a key component in outreach events to promote the benefits of STEM (science, technology, engineering, and mathematics) fields and the students' respective career opportunities. Your company logo will also appear with the team in the view of its competitors, thereby expanding your company's presence among students with design and hands-on technical experience. The Industrial Relations arm of the Purdue IEEE Student Organization will also be available for hosting and publicizing your visits to campus.
+        <hr />
+        <h2>Current Sponsers</h2>
+        <!-- TODO add list of current sponsers -->
+        <p>
+        The sponsers for the 2020-21 competition year.
         </p>
-        <br />
-        <div style="padding-left: 34%;">
-            <div class="center-box">
-                <h2 id="events">CONTACT:</h2>
-                <h4 id="events">Alex Zhang</h4>
-                <h4 id="events">Sponsorship Coordinator</h4>
-                <h4 id="events"><a href="mailto:zhan2926@purdue.edu">do18@purdue.edu</a>
-            </div>
 
+        <hr />
+        <h2>Sponsership Levels</h2>
+        <p>
+        We recognize corporate sponsers in the following ways:
+        </p>
+
+        <hr />
+        <h2>Become a Sponser</h2>
+        <p>
+        By supporting Purdue Aerial Robotics Team IEEE, your company will be helping 
+            passionate and motivated students gain valuable experience that will enhance 
+            their technical and professional skills. The UAS will be a key component 
+            in outreach events to promote the benefits of STEM (science, technology, 
+            engineering, and mathematics) fields and the students' respective career 
+            opportunities. Your company logo will also appear with the team in the 
+            view of its competitors, thereby expanding your company's presence among 
+            students with design and hands-on technical experience. The Industrial Relations 
+            arm of the Purdue IEEE Student Organization will also be available for hosting 
+            and publicizing your visits to campus.
+        </p>
+        <div class="center-box">
+            <h2 id="events">Contact:</h2>
+            <h4 id="events">Alex Zhang</h4>
+            <h4 id="events">Sponsorship Coordinator</h4>
+            <h4 id="events"><a href="mailto:zhan2926@purdue.edu">zhan2926@purdue.edu</a>
         </div>
-        <br />
+        </div>
     </div>
 </div>
 
